ab8f94cc766d3b2c8d3b28c35b1b358d73fb30d2
Eron Luna <eronluna@yahoo.com>
2019-02-15 14:28:16 -0600

    heroku

--- myapps/settings.py
+++ myapps/settings.py
@@ -128,6 +128,6 @@ STATICFILES_DIRS = [
              os.path.join(BASE_DIR, 'static'),
         ]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
